072d0779a0018e77c549e1d96ff82a426f81c812
James Yoo <jyoo980@gmail.com>
2019-09-27 22:30:44 -0700

    adding upates to FileSystem specfile

--- test/util/FileSystem.spec.ts
+++ test/util/FileSystem.spec.ts
@@ -4,25 +4,24 @@ import { expect } from "chai";
 
 describe("FileSystem write/delete tests", () => {
 
-    const testDir: string = "./test/codegen";
+    const testDir: string = "./codegen/test";
+    const createdFiles: string[] = [];
     let fs: FileSystem;
 
     before(() => {
-        fs = new FileSystem(testDir);
+        fs = new FileSystem();
     });
 
-    after(async() => {
+    after(async () => {
         try {
-            const fileNames = await nodeFs.readdir(testDir);
-            const filesToDelete: string[] = fileNames.map((name: string) => `${testDir}/${name}.ts`);
-            const deleteOps: Array<Promise<void>> = filesToDelete.map((toDelete: string) => nodeFs.unlink(toDelete));
+            const deleteOps: Array<Promise<void>> = createdFiles.map((toDelete: string) => nodeFs.unlink(toDelete));
             await Promise.all(deleteOps);
         } catch (err) {
             // TODO: make logging class
         }
     });
 
-    it("should successfully write a file (one level deep, e.g. codegen/Foo.ts", async () => {
+    it("should successfully write a file (one level deep, e.g. codegen/test/Foo.ts", async () => {
         const file: string = "Foo";
         let result: string;
         try {
@@ -30,7 +29,21 @@ describe("FileSystem write/delete tests", () => {
         } catch (err) {
             result = err;
         } finally {
-            expect(result).to.equal("./test/codegen/Foo.ts");
+            expect(result).to.equal("./codegen/test/Foo.ts");
+            createdFiles.push(result);
+        }
+    });
+
+    it("should successfully write a file (> 1 level deep, e.g. codegen/test/src/InsightFacade.ts", async () => {
+        const file: string = "InsightFacade";
+        let result: string;
+        try {
+            result = await fs.generateFile(file, "./codegen/test/src", "test");
+        } catch (err) {
+            result = err;
+        } finally {
+            expect(result).to.equal("./codegen/test/src/InsightFacade.ts");
+            createdFiles.push(result);
         }
     });
 });
