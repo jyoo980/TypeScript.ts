b4de5048f9d322a4a93a499449aed45cf7dc8963
James Yoo <jyoo980@gmail.com>
2019-10-03 15:46:04 -0700

    adding class comment for ParamDecl

--- src/ast/ParamDecl.ts
+++ src/ast/ParamDecl.ts
@@ -1,6 +1,13 @@
 import {AstNode} from "./AstNode";
 import {VarList} from "./VarList";
 
+/**
+ * Represents a parameter declaration AST node
+ *
+ * Take a look at the VarList class to see how this works, basically looks like
+ *
+ * [<type> name, <type> name, etc...]
+ */
 export default class ParamDecl extends AstNode {
 
     params: VarList;
