8f21a9c6c110426ad2ebb2a958bd76840b2860f2
kiyomih <j.kiyomi.henry@alumni.ubc.ca>
2019-10-05 15:34:43 -0700

    update Tokenizer initialization to throw Errors

--- src/util/Tokenizer.ts
+++ src/util/Tokenizer.ts
@@ -1,18 +1,35 @@
+export class TokenizerError extends Error {
+    constructor(...args: any[]) {
+        super(...args);
+        Error.captureStackTrace(this, TokenizerError);
+    }
+}
+
 export class Tokenizer {
     private static tokenizerInstance: Tokenizer;
+    private static tokenizerFile: string;
     private tokens: string[];
 
     private constructor(fileName: string) {
+        Tokenizer.tokenizerFile =  fileName;
         // TODO: implement this
     }
 
     public static makeInstance(fileName: string) {
         if(!this.tokenizerInstance) {
             this.tokenizerInstance = new Tokenizer(fileName);
+        } else {
+            const msg = `Called makeInstance with ${fileName},but already initialized with ${this.tokenizerFile}`;
+            console.warn(msg);
         }
     }
 
     public static getInstance(): Tokenizer {
+        if(!this.tokenizerInstance) {
+            const msg = `Must initialize Tokenizer with Tokenizer::makeInstance before calling Tokenizer::getInstance`;
+            console.warn(msg);
+            throw new TokenizerError(msg);
+        }
         return this.tokenizerInstance;
     }
 
