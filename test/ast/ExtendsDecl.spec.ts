1f53bf6e39c6b3c03da0b30e9d1542327bd6d9c1
kiyomih <j.kiyomi.henry@alumni.ubc.ca>
2019-10-08 14:32:21 -0700

    parse for extends and implements

--- test/ast/ExtendsDecl.spec.ts
+++ test/ast/ExtendsDecl.spec.ts
@@ -6,8 +6,7 @@ describe("ExtendsDecl parse test", () => {
     it("parses extends type line", () => {
         let tokenizer : Tokenizer = new Tokenizer("extends.txt", "./test/testFiles");
         let extendsDec : ExtendsDecl = new ExtendsDecl();
-        expect(JSON.stringify(extendsDec.parse(tokenizer)))
-            .to.equal("{\"typeTable\":{\"table\":{}},\"parentName\":\"Animal\"}");
+        expect(extendsDec.parse(tokenizer).parentName).to.equal("Animal");
     });
 
 });
