2d1012492c8ac8aba06164b82e644f0ec01af4e5
James Yoo <jyoo980@gmail.com>
2019-10-07 18:39:28 -0700

    adding initial implementation of FuncDecl

--- src/ast/FuncDecl.ts
+++ src/ast/FuncDecl.ts
@@ -9,26 +9,62 @@ import {Tokenizer} from "../util/Tokenizer";
  * e.g. function modifier (async | static)? str
  *          params []
  *          comments?
- *          (generate getter)?
- *          (generate setter)?
  *          (return TYPE)?
  */
 export default class FuncDecl extends AstNode {
 
     modifier: string;
-    isAsync: boolean;
-    isStatic: boolean;
+    isAsync: boolean = false;
+    isStatic: boolean = false;
     name: string;
     params: VarList;
     comment: CommentDecl;
-    generateGetter: boolean;
-    generateSetter: boolean;
     returnType: string;
 
     public parse(context: Tokenizer): any {
+        let indentLevel: number = context.getCurrentLineTabLevel();
+        context.getAndCheckNext("function");
+        this.modifier = context.getNext();
+        const modifierOrName = context.getNext();
+        if (this.isModifier(modifierOrName)) {
+            this.parseAfterModifier(context, modifierOrName, indentLevel);
+        } else {
+            this.parseNoModifiers(context, modifierOrName, indentLevel);
+        }
+    }
+
+    private parseAfterModifier(context: Tokenizer, modifier: string, indentLevel: number): any {
+        if (modifier === "static") {
+            this.isStatic = true;
+        } else {
+            this.isAsync = true;
+        }
+        this.name = context.getNext();
+        // TODO
+    }
+
+    private parseNoModifiers(context: Tokenizer, name: string, indentLevel: number): any {
+        this.name = name;
+        if (context.getCurrentLineTabLevel() <= indentLevel) return;
+        context.getAndCheckNext("params");
         this.params = new VarList();
-        this.comment = new CommentDecl();
-        // TODO: implement the rest.
+        this.params.parse(context);
+
+        const commentOrReturn = context.getNext();
+        if (commentOrReturn === "comments") {
+            this.comment = new CommentDecl();
+            // TODO: parse return (if it has any)
+        } else if (commentOrReturn === "returns") {
+            this.returnType = context.getNext();
+            this.comment = null;
+            return;
+        }
+        this.comment = null;
+        this.returnType = "void";
+    }
+
+    private isModifier(modifierOrName: string): boolean {
+        return (modifierOrName === "async" || modifierOrName === "static");
     }
 
     public evaluate(): any {
---
+++ test/ast/FuncDecl.spec.ts
@@ -0,0 +1,39 @@
+import { expect } from "chai";
+import FuncDecl from "../../src/ast/FuncDecl";
+import {Tokenizer} from "../../src/util/Tokenizer";
+
+describe("FuncDecl parse tests", () => {
+
+    let funcDecl: FuncDecl;
+
+    before(() => {
+        funcDecl = new FuncDecl();
+    });
+
+    it("should parse a simple function declaration (no params)", () => {
+        // private foo(): number;
+        const tokenizer: Tokenizer = new Tokenizer("funcDeclSimple.txt", "./test/testFiles");
+        funcDecl.parse(tokenizer);
+        expect(funcDecl.modifier).to.equal("private");
+        expect(funcDecl.name).to.equal("foo");
+        expect(funcDecl.params.nameToType.size).to.equal(0);
+        expect(funcDecl.isAsync).to.equal(false);
+        expect(funcDecl.isStatic).to.equal(false);
+        expect(funcDecl.comment).to.equal(null);
+        expect(funcDecl.returnType).to.equal("number");
+    });
+
+    it("should parse a parameterized function declaration", () => {
+        // public bar(facade: InsightFacade, foo: number): void;
+        const tokenizer: Tokenizer = new Tokenizer("funcDeclParameterized.txt", "./test/testFiles");
+        funcDecl.parse(tokenizer);
+        expect(funcDecl.modifier).to.equal("public");
+        expect(funcDecl.name).to.equal("bar");
+        expect(funcDecl.params.nameToType.size).to.equal(2);
+        expect(funcDecl.isAsync).to.equal(false);
+        expect(funcDecl.isStatic).to.equal(false);
+        expect(funcDecl.comment).to.equal(null);
+        expect(funcDecl.returnType).to.equal("void");
+    });
+
+});
